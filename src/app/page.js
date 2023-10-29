9cf77554215337335068914b2943681149a5c16d
jayen123 <jayenhasan2003@gmail.com>
2023-10-29 22:04:28 +0600

    3

--- src/app/page.js
+++ src/app/page.js
@@ -1,23 +1,37 @@
+"use client"
+import { useState } from "react"
 import Image from "next/image"
+import Plus from "../../public/plus.png"
+import Student from "../../public/student.jpg"
+import Student2 from "../../public/student2.jpg"
+import Circle from "../../public/half-circle.png"
+import { motion } from "framer-motion"
 export default function Home() {
   return (
       <>
         <section>
             <div className='container'>
-                <div>
-                    <div>
-                        <h1>InVesting   In knowledge and your future</h1>
+                <div className="grid grid-cols-2 items-center">
+                    <div className="grid gap-5">
+                        <motion.h1
+                        initial={{y: -20, opacity: 0, scale: .9}}
+                        animate={{y: 0, opacity: 1, scale: 1}}
+                        transition={{duration: 1}}
+                         className="text-7xl font-bold">Empowering <br/>  Young Minds For <br/> A Digital future</motion.h1>
                         <p>
-                          Saving World with Good Designs. Because your satisfaction is everything.
+                        Welcome to Eduvbhai, where we empower <br/> junior students eith cutting-edge skills to <br/>
+                        thrive in the digital age.
                         </p>
-                        <button>Get Started</button>
+                        <button className="w-1/5 px-5 py-2 text-white bg-teal-500 rounded text-lg">Get Started</button>
                     </div>
-                    <div>
-                        <div>
-                            <Image src="/plus.png" width={50} height={50} alt="plus"/>
+                    <div className="flex flex-col">
+                        <div className="flex justify-around items-center">
+                            <Image src={Plus} alt="plus"/>
+                            <Image src={Student} alt="student"/>
                         </div>
-                        <div>
-                            <></>
+                        <div className="flex relative">
+                            <Image className="rounded-full" src={Student2} alt="student"/>
+                            <Image className="absolute left-40 top-[-9%]" src={Circle} alt="circle"/>
                         </div>
                     </div>
                 </div>
